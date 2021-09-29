1384f6c07a9f521ed95ef72ac436434b639226c0
tushargoyal1320 <54525865+tushargoyal1320@users.noreply.github.com>
2021-09-29 16:37:14 +0530

    Add files via upload

---
+++ 9-path-module.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+
+const filePath = path.join('/content','subfolder','test.txt');
+console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base)
+
+const absolutePath = path.resolve(__dirname,'content','subfolder','test.txt');
+console.log(absolutePath);
